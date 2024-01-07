7f224fb28a074ed2d6afecda321ef87e599a963c
ScuttoZ <96155174+ScuttoZ@users.noreply.github.com>
2024-01-07 17:02:43 +0100

    Added getblockfilter and minor fixes

--- PyBitcoincore_SSH.py
+++ PyBitcoincore_SSH.py
@@ -18,27 +18,27 @@ import pprint
 ssh_host = '192.168.0.19'
 ssh_port = 22
 ssh_user = 'admin'
-ssh_password = 'Scutti97'
+ssh_password = 'raspiblitz'
 
 # CONNECT
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(ssh_host, ssh_port, ssh_user, ssh_password)
 
-# GetBlockHash                                                          # Returns the hash (str) of the best (tip) block in the most-work fully-validated chain.
-def getblockhash(block_height):
-    command = f'bitcoin-cli getblockhash {block_height}'
+# GetBestBlockHash                                                          # Returns the hash (str) of the best (tip) block in the most-work fully-validated chain.
+def getbestblockhash():
+    command = f'bitcoin-cli getbestblockhash'
     stdin, stdout, stderr = ssh.exec_command(command)
     output = stdout.read().decode('utf-8')
     return(output) 
 
-# GetBlock                                                              # If verbosity is 0, returns a string (str) that is serialized, hex-encoded data for block ‘block_hash’.
-def getblock(block_hash, verbosity=1):                                # If verbosity is 1, returns an object (dict) with information about block ‘block_hash’.
-    command = f'bitcoin-cli getblock "{block_hash}" {verbosity}'        # If verbosity is 2, returns an object (dict) with information about block ‘block_hash’
-    stdin, stdout, stderr = ssh.exec_command(command)                   #  and information about each transaction.
+# GetBlock                                                                  # If verbosity is 0, returns a string (str) that is serialized, hex-encoded data for block ‘block_hash’.
+def getblock(block_hash, verbosity=1):                                      # If verbosity is 1, returns an object (dict) with information about block ‘block_hash’.
+    command = f'bitcoin-cli getblock {block_hash} {verbosity}'              # If verbosity is 2, returns an object (dict) with information about block ‘block_hash’
+    stdin, stdout, stderr = ssh.exec_command(command)                       #  and information about each transaction.
     output = stdout.read().decode('utf-8')    
-    if verbosity == 0:                                                      # Translation from hex / json to human-readable text customized on the verbosity parameter
-        print(output)                                                       # (totally optional, to remove the "print"s comment out line 41 and lines 44 to 55)
+    if verbosity == 0:                                                          # Translation from hex / json to human-readable text customized on the verbosity parameter
+        print(output)                                                           # (totally optional, to remove the "print"s comment out line 41 and lines 44 to 55)
         return(output)
     load = json.loads(output)                                                  
     if verbosity == 1:
@@ -61,9 +61,9 @@ def getblockchaininfo():
     stdin, stdout, stderr = ssh.exec_command(command)                  
     output = stdout.read().decode('utf-8')
     load = json.loads(output)
-    for key in load:                                                            # 
-            if key != 'tx':                                                     # Translation to human-readable text (optional, lines 64 to 66)
-                print(key + ': ' + str(load[key]))                              #
+    for key in load:                                                             # 
+            if key != 'tx':                                                      # Translation to human-readable text (optional, lines 64 to 66)
+                print(key + ': ' + str(load[key]))                               #
     return(load)
             
 # GetBlockCount                                                              # Returns the height (int) of the most-work fully-validated chain.
@@ -73,16 +73,41 @@ def getblockcount():
     output = stdout.read().decode('utf-8')
     return(output)
 
-# GetBlockFilter                                                             # Retrieve a BIP 157 content filter for a particular block.
-#def getblockfilter(block_hash, filter_type='basic'):
-#    command = f'bitcoin-cli getblockfilter {block_hash} {filter_type}'       
+# GetBlockFilter                                                             # Retrieve a BIP 157 content filter (dict) for a particular block. 
+def getblockfilter(block_hash, filter_type='basic'):                         # (requires blockfilterindex=basic in bitcoin.conf file)
+    command = f'bitcoin-cli getblockfilter {block_hash} {filter_type}'       
+    stdin, stdout, stderr = ssh.exec_command(command)                  
+    output = stdout.read().decode('utf-8')
+    load = json.loads(output)
+    for key in load:                                                             # Translation to human-readable text (optional, lines 82-83)
+        print(key + ': ' + str(load[key])) 
+    return(load)
 
+# GetBlockHash                                                               # Returns hash (str) of block in best-block-chain at height provided.
+def getblockhash(block_height):
+    command = f'bitcoin-cli getblockhash {block_height}'
+    stdin, stdout, stderr = ssh.exec_command(command)
+    output = stdout.read().decode('utf-8')
+    return(output) 
+    
+#GetBlockHeader
+def getblockheader(block_hash, verbose='true'):                               # If verbose is false, returns a string (str) that is serialized, hex-encoded data for blockheader ‘hash’.
+    command = f'bitcoin-cli getblockheader {block_hash} {verbose}'            # If verbose is true, returns an Object (dict) with information about blockheader ‘hash’.
+    stdin, stdout, stderr = ssh.exec_command(command)
+    output = stdout.read().decode('utf-8')                                        # [WARNING] Passing verbose as a boolean raises error, error handling to be added
+    if verbose == 'false':
+        print(output)                                                             # Translation from hex / json to human-readable text customized on the verbosity parameter
+        return(output)                                                            # (optional, lines 99 and 102 to 107)
+    load = json.loads(output)
+    if verbose == 'true':
+        for key in load:
+            if key != 'tx':
+                print(key + ': ' + str(load[key]))
+            else:  
+                print(key + ':\n  ' + str(load[key]).replace(',','\n '))
+    return(load)
+    
+#GetBlockStats
 
-
-
-
-#getblockhash(0)
-getblockfilter('00000000c937983704a73af28acdec37b049d214adbda81d7e2a3dd146f6ed09')
-#getblockchaininfo()
 
 ssh.close()
