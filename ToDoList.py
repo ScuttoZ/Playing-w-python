377ae373707bcaf097da25392a8467d4f6503299
ScuttoZ <96155174+ScuttoZ@users.noreply.github.com>
2024-01-22 00:40:25 +0100

    Added bare bones Read (once Show) and Edit frunctionalities

--- ToDoList.py
+++ ToDoList.py
@@ -1,38 +1,71 @@
 # TO DO LIST ## Command Line # Focus: readable code
-# v.1 is the classic if-giganest that you code in high school. Neither classy nor efficient, but works.
+# v.1 is the classic if-giganest that you code in high school. Neither classy nor efficient, but works (on PC).
 
-#class myList():
-#    def __init__(self) -> None:
-#        self.name = input('What''s it called?')
-#        self.all_done = False
-#        self.itemsStored = {}                                                          [WORK IN PROGRESS] I want to upgrade the code 
-#        print(f'New list created: {self.name}')                                            to allow for multiple lists to exist without 
-#                                                                                           the need for multiple text files.
-#    def addItem(self, item):
-#        self.itemsStored = self.itemsStored.update({item.id : item})
-#        print(f'item added: {item.content}')
-#                  
-#class myItem:
-#    def __init__(self, content) -> None:
-#        self.id = len(a.itemsStored)
-#        self.content = content
-#        self.done = False
         
-#used_names = []
+# Initialize at launch
+import subprocess
+import platform
+
+usedNames = []
 illegalChars = ['/','\\','|','<','>',':','?','*']
-action = input('What''s up?\nC = Create new list\nS = Show lists list\nE = Edit a list\n')
+running = True
 
-#while action.upper() != '':            implement a loop to perform more actions for each run
-if action.upper() == 'C':
-    newList = input('How shall the new list be called?\n')    #add check on names already existing 
-    if any(char in newList for char in illegalChars):
-        print('An illegal character was used. I''m calling the police.')
-    else:
+# Loops this while running.
+while running:            
+    action = input('_______________________________________________________________________________________________________\nWhat''s up?\nC = Create new list\nR = Read a list\nE = Edit a list\n')
+    
+    # Create
+    if action.upper() == 'C':
+        newList = input('How shall the new list be called?\n')    #add check on names already existing 
+        if any(char in newList for char in illegalChars):
+            print('An illegal character was used. I''m calling the police.')
+        else:
+            try:
+                with open(f'{newList}.txt', 'a') as lst:
+                    lst.write(f'--- {newList} ---\n')
+                    print(f'New list created: {newList}\n')
+                    action = ''
+                    answer = input('Do you want to write in it?\nY / N')
+                    if answer.upper() == 'Y':
+                        action = 'E'
+                    elif answer.upper() == 'N':
+                        action = ''
+            except:
+                print('How did you get here?')
+                action = ''
+    
+    # Read
+    if action.upper() == 'S':
+        selection = input('Which list do I open?\n')           # User must know which lists exist. Add list of lists to display when 
         try:
-            with open(f'{newList}.txt', 'a') as lst:
-                lst.write(f'--- {newList} ---\n')
-                print(f'New list created: {newList}\n')
-                #answer = input('Do you want to add items to it?')   [WORK IN PROGRESS]
+            with open(f'{selection}.txt') as lst:
+                print(f'-----BEGIN TXT FILE-----')
+                print(f'{lst.read()}')
+                print('-----END TXT FILE-----')
+                action = ''
         except:
-            print('How did you get here?')
+            print('Couldn''t open soz\n')
+            action = ''
+    
+    # Edit
+    if action.upper() == 'E':
+        selection = input('Which list ae we editing?\n')
+        try:
+            if platform.system() == "Windows":
+                command = 'notepad'
+            elif platform.system() == "Darwin":
+                command = 'open'
+            else:
+                command = 'xdg-open'
+            subprocess.run([command, f'{selection}.txt'], check=True)
+            action = ''
+        except:
+            print('Are you running this on mobile??\n')
+            action = ''
+    
+    # Close
+    running = False if input('Are you done?\nY / N') == 'Y' else True
+    print('Alright, back to the menu:\n')
 
+
+## aggiungere possibilità di cifrare i file con una password che non viene salvata dallo script ma solo presa in input a ogni run
